d7b042bb36c164734132090fe35e1b6d8b4c8d20
Sergio Ríos <riosfsergio@gmail.com>
2020-03-21 19:50:38 +0100

    Component avatar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="avatar">
+    <img alt="Avatar" src="../assets/me.jpeg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar'
+}
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+    border-radius: 100%;
+    height: 200px;    
+    overflow: hidden;
+    width: 200px;
+
+    img {
+        display: block;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+}
+</style>
