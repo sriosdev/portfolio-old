346ab6ab4e7860c6a2d732d79f338ebdcc466c85
Sergio Ríos <riosfsergio@gmail.com>
2020-03-24 20:16:04 +0100

    View contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Contact</p>
+  <div>
+    <h2>Contact me</h2>
+    <p><a href="https://www.linkedin.com/in/sergio-riosf/"><i class="fab fa-linkedin"></i> /sergio-riosf</a></p>
+    <p><a href="https://twitter.com/sriosdev"><i class="fab fa-twitter"></i> @sriosdev</a></p>
+    <p><a href="https://github.com/sriosdev"><i class="fab fa-github"></i> sriosdev</a></p>
+    <p><a href="mailto:riosfsergio@gmail.com"><i class="fas fa-envelope"></i> riosfsergio@gmail.com</a></p>
+  </div>
 </template>
 
 <script>
@@ -8,6 +14,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
 </style>
