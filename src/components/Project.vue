5ed889b130283d078624550e4a17f8051893bf6e
Sergio Ríos <riosfsergio@gmail.com>
2020-03-26 21:12:30 +0100

    Projects section

---
+++ src/components/Project.vue
@@ -0,0 +1,70 @@
+<template>
+  <a :href="url" target="_blank">
+    <div class="project">
+      <img :src="require(`@/assets/${image}`)" alt="Project" />
+      <div class="text">
+        <h3>{{ name }}</h3>
+        <span>{{ description }}</span>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: {
+    name: String,
+    description: String,
+    image: String,
+    url: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+
+a {
+  color: $color-text;
+  text-decoration: none;
+}
+
+h3 {
+  margin: 0;
+}
+
+.project {
+  border-radius: 1em;
+  -webkit-box-shadow: 5px 0px 20px -5px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 5px 0px 20px -5px rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 0px 20px -5px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  height: 25em;
+  transition: all 0.5s;
+  width: 25em;
+
+  &:hover {
+    -webkit-box-shadow: 0px 0px 40px -5px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 0px 0px 40px -5px rgba(0, 0, 0, 0.5);
+    box-shadow: 0px 0px 40px -5px rgba(0, 0, 0, 0.5);
+  }
+
+  img {
+    display: block;
+    height: 65%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .text {
+    border-top: 5px solid $color-secondary;
+
+    height: 35%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,47 @@
 <template>
-  <p>Projects</p>
+  <div>
+    <h2>Personal projects</h2>
+    <div class="projects">
+      <Project
+        class="project"
+        v-for="(project, index) in projects"
+        :key="index"
+        :name="project.name"
+        :description="project.description"
+        :image="project.img"
+        :url="project.url" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Project from '../components/Project.vue'
+import json from '../resources/projects.json';
+
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  components: {
+    Project
+  },
+  data: function() {
+    return {
+      projects: json 
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
 
+.projects {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom : 5em;
+
+  .project {
+    margin: 1em;
+  }
+}
 </style>
