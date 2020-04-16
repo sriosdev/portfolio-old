8ab6c4b1d52f693dc01added8f67f41ec9b6d154
Sergio Ríos <riosfsergio@gmail.com>
2020-04-16 18:13:27 +0200

    Fix typo in about me

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about-me">
     <h2>About me</h2>
-    <p>Since I was a child, I've een an enthusiast of computers. One day I came across programming, and that's when I decided I wanted to become a developer.</p>
+    <p>Since I was a child, I've been an enthusiast of computers. One day I came across programming, and that's when I decided I wanted to become a developer.</p>
     <p>I've studied web application development and I'm currently working as a developer in a tech company.</p>
     <p>In the last few years, I've developed traditional web apps and RESTful APIs using PHP, JavaScript and MySQL. I've also used frameworks such as Laravel, Symfony and Vue.js, professionally and on my personal projects.</p>
     <p>Today, I am focusing my studies on Go, the benefits of concurrent programming, and I'm always happy to explore new things.</p>
