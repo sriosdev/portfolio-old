b44232b0c8084d076b0bf82f94df4234b221434a
Sergio Ríos <riosfsergio@gmail.com>
2020-04-18 13:05:48 +0200

    New personal logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="../assets/logo_sr_descripcion.png" alt="Logo">
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 225px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="home">
-    <h2>Welcome!</h2>
-    <p>Hello! My name is Sergio.</p>
-    <p>Here you'll find some information about me and my projects.</p>
+    <Logo class="logo" />
+    <p>Welcome! Here you'll find some information about me and my projects.</p>
   </div>
 </template>
 
 <script>
+import Logo from '../components/Logo.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Logo
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   div.home {
-    margin-bottom: 5em;
     margin-left: auto;
     margin-right: auto;
     width: 30em;
+
+    .logo {
+      margin-top: 1.8em;
+      margin-bottom: 1em;
+    }
   }
 
   @media (min-width: 320px) and (max-width: 480px) {
